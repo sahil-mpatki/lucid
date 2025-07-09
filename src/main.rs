7ea61e247cd1f07269fb2e82280434b5d2f47c08
Sahil Patki <sahilpatki@Jojo.local>
2025-07-09 20:41:00 +0530

    Found some crashes with the naive fuzzer!! :)))

--- src/main.rs
+++ src/main.rs
@@ -6,72 +6,67 @@ use std::fs;
 use std::os::unix::process::ExitStatusExt;
 use std::process::Command;
 
-fn write_to_disk(data: Vec<u8>) {
+fn write_to_disk(data: Vec<u8>) -> String {
     let mut rng = rand::rng();
-    let _ = fs::write(
-        format!(
-            "/Users/sahilpatki/woot/lucid/out/canon_40D_mutated_{}.jpg",
-            rng.random_range(0..10000)
-        ),
-        data,
+    let file_name = format!(
+        "/Users/sahilpatki/woot/lucid/out/canon_40D_mutated_{}.jpg",
+        rng.random_range(0..10000)
     );
+    let _ = fs::write(file_name.clone(), data);
+
+    return file_name;
 }
 
-fn mutate() {
-    let data = fs::read("/Users/sahilpatki/woot/lucid/data/canon_40D.jpg").unwrap();
-    let mut mutation_data = data.clone();
-    // let mut mutation_data_2 = data.clone();
+fn read_file(abs_path: &str) -> Vec<u8> {
+    let data = fs::read(abs_path).unwrap();
+    return data;
+}
 
+fn mutate(data: &mut Vec<u8>) {
     // mutate the data
-    bitflip(&mut mutation_data);
-    gynvaels_magic_bytes(&mut mutation_data);
+    bitflip(data);
+    gynvaels_magic_bytes(data);
 
-    write_to_disk(mutation_data);
+    // write_to_disk(mutation_data);
     // write_to_disk(mutation_data_2);
 }
 
-fn run() {
-    let files = fs::read_dir("/Users/sahilpatki/woot/lucid/out/").unwrap();
-    for mut file in files {
-        let output = Command::new("/Users/sahilpatki/woot/lucid/test_target/exif")
-            .arg(
-                "/Users/sahilpatki/woot/lucid/out/".to_string()
-                    + file
-                        .as_mut()
-                        .unwrap()
-                        .file_name()
-                        .into_string()
-                        .unwrap()
-                        .as_str(),
+fn run(file_name: String) {
+    let output = Command::new("/Users/sahilpatki/woot/lucid/test_target/exif")
+        .arg(file_name.clone())
+        .output()
+        .expect("Failed to execute program!");
+
+    let status = output.status;
+
+    if let Some(signal) = status.signal() {
+        if signal == 11 {
+            println!("CRASH FOUND: {}", file_name.clone());
+            let data = fs::read(file_name.clone()).unwrap();
+            let split_file_name: Vec<&str> = file_name.split('/').collect();
+            let just_file_name = split_file_name[split_file_name.len() - 1];
+            fs::write(
+                format!("/Users/sahilpatki/woot/lucid/crashes/{}", just_file_name),
+                data,
             )
-            .output()
-            .expect("Failed to execute program!");
-
-        let status = output.status;
-
-        if let Some(signal) = status.signal() {
-            if signal == 11 {
-                println!(
-                    "CRASH FOUND: {}",
-                    file.as_mut()
-                        .unwrap()
-                        .file_name()
-                        .into_string()
-                        .unwrap()
-                        .as_str()
-                );
-            }
+            .unwrap();
         }
     }
 }
 
 fn fuzz() {
     loop {
+        // read file
+        let mut data = read_file("/Users/sahilpatki/woot/lucid/data/canon_40D.jpg");
+
         // mutate
-        mutate();
+        mutate(&mut data);
+
+        // write to the a file
+        let out_name = write_to_disk(data);
 
         // feed it into the target program
-        run();
+        run(out_name);
     }
 }
 
